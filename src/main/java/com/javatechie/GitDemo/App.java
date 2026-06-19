10f357bfc922c2e260905959937e54a2370f43d5
Genesis-Sasi <LondonBridge@123>
2026-06-18 23:36:44 -0400

    added gitdemo to local repository

---
+++ src/main/java/com/javatechie/GitDemo/App.java
@@ -0,0 +1,19 @@
+package com.javatechie.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void methodOne() {
+		System.out.println("Welecoming sasi to Git");
+		
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
